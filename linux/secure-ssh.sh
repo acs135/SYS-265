5d25023830353b0addc67dad85cdc8ff84e470a0
root <acs135@yahoo.com>
2025-02-19 03:04:35 +0000

    test success

--- linux/secure-ssh.sh
+++ linux/secure-ssh.sh
@@ -3,7 +3,7 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-if [ "$EUID -ne 0 ]
+if [ "$EUID" -ne 0 ]
 	then echo "Please run as root"
 	exit
 fi
